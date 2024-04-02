1af029f9db646c595b66db67810ab6aac7cd4659
ronald-abreu <ronaldnascimento2@hotmail.com>
2024-04-01 22:03:05 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("homepage.html")
+
+@app.route('/perfil/<usuario>')
+def perfil(usuario):
+    return render_template("perfil.html", usuario = usuario)
+
+if __name__ == "__main__":
+    app.run(debug=True)
