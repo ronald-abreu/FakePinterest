df981246c9118f61787e15273e780e8aec9c0e5d
ronald-abreu <ronaldnascimento2@hotmail.com>
2024-04-04 22:25:37 -0300

    reestruturando o código e adicionando novs arquivos

---
+++ fakepinterest/routes.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, url_for
+from fakepinterest import app
+
+@app.route('/')
+def homepage():
+    return render_template("homepage.html")
+
+@app.route('/perfil/<usuario>')
+def perfil(usuario):
+    return render_template("perfil.html", usuario = usuario)
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template, url_for
-
-app = Flask(__name__)
-
-@app.route('/')
-def homepage():
-    return render_template("homepage.html")
-
-@app.route('/perfil/<usuario>')
-def perfil(usuario):
-    return render_template("perfil.html", usuario = usuario)
+from fakepinterest import app
 
 if __name__ == "__main__":
     app.run(debug=True)
