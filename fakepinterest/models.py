27a203753e5031e7c40c020f07e5492eaa92b606
ronald-abreu <ronaldnascimento2@hotmail.com>
2024-04-05 20:31:52 -0300

    Criação das estruturas do banco de dados e criação do banco de dados

---
+++ fakepinterest/models.py
@@ -0,0 +1,15 @@
+from fakepinterest import database
+from datetime import datetime
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+    fotos = database.relationship("Foto", backref="usuario", lazy=True)
+
+class Foto(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    imagem = database.Column(database.String, default="default.png")
+    data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow())
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
