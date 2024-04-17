e02ea0a1b738b7e0993b0f1e5b3cb7f0368ae643
ronald-abreu <ronaldnascimento04@outlook.com>
2024-04-16 21:08:53 -0300

    Correção do login e criação do banco de dados Postgresql

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -13,7 +13,7 @@ def homepage():
     form_login = FormLogin()
     if form_login.validate_on_submit():
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(usuario.senha.encode("utf-8"), form_login.senha.data):
             login_user(usuario)
             return redirect(url_for('perfil', id_usuario=usuario.id))
     return render_template("homepage.html", form = form_login)
@@ -22,7 +22,7 @@ def homepage():
 def criar_conta():
     form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit():
-        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf-8")
         usuario = Usuario(username=form_criarconta.username.data,
                           senha=senha,
                           email=form_criarconta.email.data)
